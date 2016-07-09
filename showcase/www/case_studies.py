5e21899be2139710ab2364f17b531041653a8748
Revant Nandgaonkar <revant.one@gmail.com>
2016-07-09 11:04:12 +0530

    Only list published cases

--- showcase/www/case_studies.py
+++ showcase/www/case_studies.py
@@ -3,4 +3,5 @@ import frappe
 def get_context(context):
 	context.case_studies = frappe.get_all("Case Study",
 		fields=["name", "case_name", "problem_description", "page_name"],
+		filters={"published":1},
 		order_by="name desc")
