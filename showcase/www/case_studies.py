2e16490bdf0e9573b1636e193eae479c557c1710
Revant Nandgaonkar <revant.one@gmail.com>
2016-07-22 22:11:47 +0530

    added route

--- showcase/www/case_studies.py
+++ showcase/www/case_studies.py
@@ -2,6 +2,6 @@ import frappe
 
 def get_context(context):
 	context.case_studies = frappe.get_all("Case Study",
-		fields=["name", "case_name", "case_description", "page_name", "source_code"],
+		fields=["name", "case_name", "case_description", "page_name", "source_code", "route"],
 		filters={"published":1},
 		order_by="name desc")
