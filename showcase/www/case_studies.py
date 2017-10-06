319ea356d7e5043a9382571b30a3b4822cbd2984
Revant Nandgaonkar <revant.one@gmail.com>
2017-10-06 21:02:16 +0530

    Pagination in Showcase

--- showcase/www/case_studies.py
+++ showcase/www/case_studies.py
@@ -1,7 +1,25 @@
 import frappe
 
 def get_context(context):
-	context.case_studies = frappe.get_all("Case Study",
+	if frappe.form_dict.get("page"):
+		page = int(frappe.form_dict.get("page"))
+		if page <= 0:
+			page = 1
+	else:
+		page = 1
+
+	case_studies = frappe.get_all("Case Study",
 		fields=["name", "case_name", "case_description", "page_name", "source_code", "route"],
 		filters={"published":1},
-		order_by="case_name asc")
+		order_by="case_name asc",
+		page_length=6,
+		start=(page-1)*6)
+	if len (case_studies) != 0:
+		context.case_studies = case_studies
+	all_cases = frappe.get_all("Case Study", filters={"published":1})
+	pages = len(chunks(all_cases,6))
+	context.pages = pages
+
+def chunks(seq, size):
+	l = [seq[i:i+size] for i  in range(0, len(seq), size)]
+	return l
